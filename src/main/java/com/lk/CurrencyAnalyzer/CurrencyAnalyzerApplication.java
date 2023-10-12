11f5f069ee40df3ecdfe7875bf20b8b8e39bd6bc
Aliaksei Lomats <alexeyttdeveloper@gmail.com>
2023-10-12 15:14:36 +0200

    initial commit

---
+++ src/main/java/com/lk/CurrencyAnalyzer/CurrencyAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.lk.CurrencyAnalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyAnalyzerApplication.class, args);
+	}
+
+}
