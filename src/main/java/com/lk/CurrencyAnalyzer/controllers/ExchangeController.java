203ecbbc1b5e961edde08f2c0005764e894f55ac
IntCat13 <85766091+IntCat13@users.noreply.github.com>
2023-11-28 21:29:03 +0100

    Added currency exchange controller
    Added endpoints to handle exchange service

--- src/main/java/com/lk/CurrencyAnalyzer/controllers/ExchangeController.java
+++ src/main/java/com/lk/CurrencyAnalyzer/controllers/ExchangeController.java
@@ -2,19 +2,22 @@ package com.lk.CurrencyAnalyzer.controllers;
 
 import com.lk.CurrencyAnalyzer.models.User;
 import com.lk.CurrencyAnalyzer.services.ExternalServices.RapidApiService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/exchange")
 public class ExchangeController {
 
-    @GetMapping(path = "/test")
-    public @ResponseBody String getAllUsers() {
+    @GetMapping("/exchange/{from}/{to}")
+    public String exchange(@PathVariable String from, @PathVariable String to) {
+        return RapidApiService.exchangeValue(from,to);
+    }
+    
+    @GetMapping("/initialise")
+    public String test() {
         RapidApiService.currenciesList();
-        return "Hellow wworld";
+
+        return RapidApiService.currencies.toString();
     }
 
 }
